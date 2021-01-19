930f4bf702c75e189383dff210ba86f3d17ed264
Sam Hall <mann.samantha11@gmail.com>
2021-01-19 00:58:25 -0500

    commit tree e69ec91b10938baa640eede70be3d7c38496ee66

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
-  resources :dogs
-  resources :employees
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  
+  
+  
+  
+  
+  # resources :dogs
+  # resources :employees
+  
 end
